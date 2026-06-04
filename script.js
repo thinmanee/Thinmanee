f089bdd5963e273f1b045dedd0fac88b0ae9b96c
thinmanee <ck930153@gmail.com>
2026-06-04 10:09:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+javascript// เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Portfolio ของ SOMCHAI พร้อมแล้ว!');
+});
